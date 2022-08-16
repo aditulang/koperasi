09143659dc6f92a8dc5bc58efece4ae90b29ac71
aditulang <aditulang767@gmail.com>
2022-08-16 07:50:42 +0700

    update

--- app/User.php
+++ app/User.php
@@ -47,7 +47,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'jenis_kelamin', 'jabatan', 'alamat', 'phone','nip'
     ];
 
     /**
