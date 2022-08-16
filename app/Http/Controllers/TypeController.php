3274c2d0055044639c7d552c3b0afed55a36d01e
aditulang <aditulang767@gmail.com>
2022-08-16 07:51:19 +0700

    type controler

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Type;
-
+use App\Http\Requests\TypesRequest;
 
 class TypeController extends Controller
 {
@@ -13,12 +13,32 @@ class TypeController extends Controller
         $types = Type::all();
         return view('types.index', compact('types'));        
     }
+
     public function store(TypesRequest $request)
     {
-
         Type::create($request->all());
 
         flash('Jenis pinjaman berhasil ditambahkan.');
+
+        return redirect()->route('types.index');
+    }
+
+    public function create()
+    {
+        return view('types.create');
+    }
+
+    public function edit(Type $type)
+    {
+        return view('types.edit', compact('type'));
+    }
+
+    public function update(TypesRequest $request, Type $type)
+    {
+
+        $type->update($request->all());
+
+        flash('Jenis pinjaman berhasil diperbaharui.');
 
         return redirect()->route('types.index');
     }
