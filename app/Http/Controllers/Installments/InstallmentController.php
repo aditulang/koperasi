d1ba2aa3c050ee44f4182336d8316f52eb234d38
aditulang <aditulang767@gmail.com>
2022-09-06 07:17:52 +0700

    instalment

--- app/Http/Controllers/Installments/InstallmentController.php
+++ app/Http/Controllers/Installments/InstallmentController.php
@@ -2,15 +2,39 @@
 
 namespace App\Http\Controllers\Installments;
 
+use App\Loan;
+use App\Installment;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Installment;
+use Nexmo\Laravel\Facade\Nexmo;
+
+
 
 class InstallmentController extends Controller
 {
     public function index()
-    {   
-        $data = Installment::all();
-        return view('installments.index', compact('data'));
+    {
+        $data = [
+            'loans' => Loan::with('user','type')->where('terverifikasi', true)->get(),
+        ];
+        return view('installments.index', $data);
     }
+    public function show(Loan $loan)
+    {
+        $this->authorize('show', $loan);
+        
+        return view('installments.show', compact('loan'));
+    }
+    public function create(Loan $loan)
+    {
+        $this->authorize('create', Loan::class);
+
+        $data = [
+            'loan'  => $loan,
+            'angsuran_ke'   => $loan->installments()->count() + 1,
+        ];
+
+        return view('installments.create', $data);
+    }
+    
 }
