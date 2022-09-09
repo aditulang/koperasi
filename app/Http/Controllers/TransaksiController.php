5416ce2431c1361988a987b58b2f4215683ffffe
aditulang <aditulang767@gmail.com>
2022-09-09 07:31:47 +0700

    transaksi

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -3,12 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Transaksi;
+use App\Saving;
+use App\Penarikan;
 
 class TransaksiController extends Controller
 {
     public function index()
     {
-        return view('transaksi.index');
+        $transaction = Penarikan::with('savings')->get();        
+        return view('transaksi.index', compact('transaction'));
     }
+    public function edit($id)
+    {
+        $saving = Saving::findOrFail($id);
+
+        return view('transaksi.edit', compact('saving'));
+    }
+    public function store(Request $request, $id)
+    {
+            $saving = Saving::findOrFail($id);
+
+            $penarikan = Penarikan::create($request->all());
+            if($penarikan->save()){
+                $hitung = $saving->saldo - $penarikan->total;
+
+                $saving->update([
+                    'saldo' => $hitung,
+                ]);
+            }
+            flash('Transaksi pengembalian berhasil dilakukan');
+            return redirect()->route('savings.anggota');
+    }
+
+
 }
